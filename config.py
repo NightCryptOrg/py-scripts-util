0f4e8f8022d6d1eb4a03cfec31ba52547dbf1fd1
very-amused <very-amused@pm.me>
2023-12-25 19:18:21 -0500

    Support list configs

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ import json
 import re
 
 
-def select_config(prefix: str) -> (str, dict):
+def select_config(prefix: str) -> (str, dict|list[dict]):
 	"""Select a JSON config file based on a search prefix"""
 	# Search for config files starting with prefix
 	config_dir = './config'
-	config_files: list(str) = []
+	config_files: list[str] = []
 	for file in os.listdir(config_dir):
 		if file.startswith(prefix) and file.endswith('.json'):
 			config_files.append(file)
@@ -33,14 +33,16 @@ def select_config(prefix: str) -> (str, dict):
 		with open(f'{config_dir}/{config_name}.json') as f:
 			return (config_name, json.load(f))
 
-
-def load_schema(name: str) -> dict:
+def load_schema(name: str) -> dict|list[dict]:
 	with open(f'config_examples/{name}.jsonc') as f:
-		lines: list(str) = f.readlines()
+		lines: list[str] = f.readlines()
 		lines = filter(lambda line: not line.lstrip().startswith('//'), lines)
 		return json.loads(''.join(lines))
 
-def validate_config(config: dict, schema: dict):
-	for k, v in schema.items():
-		if not k in config:
-			raise KeyError(f'Missing config key "{k}": {v}')
+def validate_config(config: dict|list[dict], schema: dict):
+	if type(config) is dict:
+		config = [config]
+	for c in config:
+		for k, v in schema.items():
+			if not k in c:
+				raise KeyError(f'Missing config key "{k}": {v}')
--- rsync.py
+++ rsync.py
@@ -7,12 +7,16 @@ class Rsync:
 	"""rsync wrapper for handling server file transfers"""
 	login: str
 	reverse: bool
+	port: int|None
 
 	def __init__(self, login: str, reverse: bool):
-		self.login = login
-		self.reverse = False if reverse is None else reverse
+		login_split = login.split(':', 2)
+		self.login = login_split[0]
+		self.port = login_split[1] if len(login_split) == 2 else None
+		self.reverse = reverse if not reverse is None else None
 
 	def __call__(self, src: str, dst: str, *args) -> subprocess.CompletedProcess:
 		rsync = 'rsync'
+		ssh_args = ['-e', f'ssh -p {self.port}'] if self.port else []
 		transfer_args = [f'{self.login}:{src}', dst] if self.reverse else [src, f'{self.login}:dst']
-		return subprocess.run([rsync] + _rsync_flags + [arg for arg in args] + transfer_args)
+		return subprocess.run([rsync] + _rsync_flags + ssh_args + [arg for arg in args] + transfer_args)
