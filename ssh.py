a3f97a1bf651d72dcd651801b9c208a29b4e1090
very-amused <very-amused@pm.me>
2023-12-27 06:04:39 -0500

    Add SSH hostname parsing

--- ssh.py
+++ ssh.py
@@ -9,3 +9,9 @@ class SSH:
 
 	def __call__(self, cmd: str, *args) -> subprocess.CompletedProcess:
 		return subprocess.run(['ssh', self.login] + [arg for arg in args] + cmd.split(' '))
+
+# Parse the hostname portion of an SSH login
+def parse_hostname(login: str) -> str:
+	hostname = login.split('@', 2)[1] if '@' in login else login
+	hostname = hostname.split(':', 2)[0] # Trim port
+	return hostname
