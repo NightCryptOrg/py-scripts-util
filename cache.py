d17f051d1c8e25cfe7fbc6af10d9ab2ca8552bff
very-amused <very-amused@pm.me>
2024-03-05 11:50:09 -0500

    Fix cache dir creation error

--- cache.py
+++ cache.py
@@ -4,6 +4,6 @@ import shutil
 def init(config_name: str) -> str:
 	"""Initialize an empty cache directory and return its path"""
 	path = f'.cache/{config_name}'
-	shutil.rmtree(path)
+	shutil.rmtree(path, ignore_errors=True)
 	os.makedirs(path)
 	return os.path.abspath(path)
--- rsync.py
+++ rsync.py
@@ -9,11 +9,11 @@ class Rsync:
 	reverse: bool
 	port: int|None
 
-	def __init__(self, login: str, reverse: bool):
+	def __init__(self, login: str, reverse=False):
 		login_split = login.split(':', 2)
 		self.login = login_split[0]
 		self.port = login_split[1] if len(login_split) == 2 else None
-		self.reverse = reverse if not reverse is None else None
+		self.reverse = reverse
 
 	def __call__(self, src: str, dst: str, *args) -> subprocess.CompletedProcess:
 		rsync = 'rsync'
