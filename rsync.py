faa03f6594090cf6247dce282db3e44e8f8711a2
very-amused <very-amused@pm.me>
2024-05-31 11:44:12 -0400

    Support local rsync transfers

--- rsync.py
+++ rsync.py
@@ -4,19 +4,27 @@ _rsync_flags = ['-h', '--info=progress2']
 
 
 class Rsync:
-	"""rsync wrapper for handling server file transfers"""
+	"""rsync wrapper for handling file transfers"""
 	login: str
+	local: bool
 	reverse: bool
 	port: int|None
 
-	def __init__(self, login: str, reverse=False):
+	def __init__(self, login: str, local=False, reverse=False):
+		self.local = local
+		if self.local: # disregard SSH login if syncing local files
+			return
 		login_split = login.split(':', 2)
 		self.login = login_split[0]
 		self.port = login_split[1] if len(login_split) == 2 else None
-		self.reverse = reverse
+		self.reserve = reserve
 
 	def __call__(self, src: str, dst: str, *args) -> subprocess.CompletedProcess:
 		rsync = 'rsync'
-		ssh_args = ['-e', f'ssh -p {self.port}'] if self.port else []
-		transfer_args = [f'{self.login}:{src}', dst] if self.reverse else [src, f'{self.login}:{dst}']
+		if self.local:
+			transfer_args = [src, dst]
+			ssh_args = []
+		else:
+			transfer_args = [f'{self.login}:{src}', dst] if self.reverse else [src, f'{self.login}:dst']
+			ssh_args = ['-e', f'ssh -p {self.port}'] if self.port else []
 		return subprocess.run([rsync] + _rsync_flags + ssh_args + [arg for arg in args] + transfer_args)
